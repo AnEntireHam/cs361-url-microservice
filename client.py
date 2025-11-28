8e5fcd0d941181b7b8388b042558519f15bd49b3
Nora Jacobi <8314702+AnEntireHam@users.noreply.github.com>
2025-11-27 17:38:37 -0800

    Add basic URL regex

--- client.py
+++ client.py
@@ -3,25 +3,35 @@ import zmq
 PORT = 6702
 context = zmq.Context()
 
+def send_message(message):
+    print(f"Sending message: {message}")
+    socket.send(message.encode())
+    response = socket.recv().decode()
+    print(f"Received response: {response}\n")
+
 if __name__ == "__main__":
     socket = context.socket(zmq.REQ)
     socket.connect(f"tcp://localhost:{PORT}")
     print(f"Timezone client running on port {PORT}.\n")
 
     message = "Hello there!"
-    print(f"Sending message: {message}")
-    socket.send(message.encode())
-    response = socket.recv().decode()
-    print(f"Received response: {response}\n")
+    send_message(message)
 
     message = "fine weather we're having today"
-    print(f"Sending message: {message}")
-    socket.send(message.encode())
-    response = socket.recv().decode()
-    print(f"Received response: {response}\n")
+    send_message(message)
 
     message = "et cetera"
-    print(f"Sending message: {message}")
-    socket.send(message.encode())
-    response = socket.recv().decode()
-    print(f"Received response: {response}\n")
+    send_message(message)
+
+    message = "http://www.example.com"
+    send_message(message)
+
+    message = "https://en.wikipedia.org"
+    send_message(message)
+
+    message = "h ttp://en.wikipedia.org"
+    send_message(message)
+
+    message = "https://en.wikipedia.org/wiki/Blood_sausage#Asia"
+    send_message(message)
+
--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import zmq
+import re
+
+# Looks for "http" or "https", then any amount of subdomains (containing alphanumeric and hyphens only,
+#   separated by "."s, ), followed by a top-level domain (again, alphanumerics and hyphens),
+#   and finally an optional section containing a "/" and then any sequence of characters (no whitespaces)
+# Is NOT guaranteed to produce a valid URL, only a valid protocol and host section.
+PATTERN_HTTPS = re.compile(r"^https?:\/\/(?:[A-z0-9-]+\.)+[A-z0-9-]+(?:\/[^\s]*)?$")
 
 context = zmq.Context()
 PORT = 6702
+
+def is_valid_host(input_url):
+    match = PATTERN_HTTPS.fullmatch(input_url)
+    return (match is not None)
 
 def server_loop(socket):
     #  Wait for next request from client
@@ -9,7 +20,9 @@ def server_loop(socket):
     message = message.decode()
     print(f"Recieved message: {message}")
 
-    response = message.upper()
+    is_valid = is_valid_host(message)
+
+    response = message + f" {is_valid}"
     socket.send(response.encode())
     print(f"Sent response: {response}")
     print()
@@ -17,7 +30,7 @@ def server_loop(socket):
 if __name__ == "__main__":
     socket = context.socket(zmq.REP)
     socket.bind(f"tcp://*:{PORT}")
-    print(f"Timezone conversion microservice running on port {PORT}.\n")
+    print(f"URL opening microservice running on port {PORT}.\n")
 
     try:
         while True:
