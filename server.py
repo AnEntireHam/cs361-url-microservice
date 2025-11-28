ba51af64aff13eb49cc7d1405982df1b02357c69
Nora Jacobi <8314702+AnEntireHam@users.noreply.github.com>
2025-11-27 18:05:05 -0800

    Added tab opening capability

--- server.py
+++ server.py
@@ -1,28 +1,46 @@
+from urllib.error import URLError
 import zmq
 import re
+import urllib.request
+import webbrowser
 
-# Looks for "http" or "https", then any amount of subdomains (containing alphanumeric and hyphens only,
+# Looks for "http://" or "https://", then any amount of subdomains (containing alphanumeric and hyphens only,
 #   separated by "."s, ), followed by a top-level domain (again, alphanumerics and hyphens),
 #   and finally an optional section containing a "/" and then any sequence of characters (no whitespaces)
 # Is NOT guaranteed to produce a valid URL, only a valid protocol and host section.
-PATTERN_HTTPS = re.compile(r"^https?:\/\/(?:[A-z0-9-]+\.)+[A-z0-9-]+(?:\/[^\s]*)?$")
+PATTERN_HOSTNAME = re.compile(
+    r"^https?:\/\/(?:[A-z0-9-]+\.)+[A-z0-9-]+(?:\/[^\s]*)?$")
+
+# Searches for "http://" or "https://"
+PATTERN_PROTOCOL = re.compile(r"^https?:\/\/")
 
 context = zmq.Context()
 PORT = 6702
 
-def is_valid_host(input_url):
-    match = PATTERN_HTTPS.fullmatch(input_url)
+def is_valid_protocol(input_url):
+    match = PATTERN_PROTOCOL.match(input_url)
     return (match is not None)
 
+def is_valid_hostname(input_url):
+    match = PATTERN_HOSTNAME.fullmatch(input_url)
+    return (match is not None)
+
+def open_url(input_url):
+    if not is_valid_protocol(input_url):
+        return 'Invalid protocol section (Should look like "http://" or "https://")'
+    if not is_valid_hostname(input_url):
+        return 'Invalid host section (Should look like "https://www.example.com")'
+    if webbrowser.open_new_tab(input_url):
+        return "Opened URL"
+    return "Invalid path or query"
+
 def server_loop(socket):
-    #  Wait for next request from client
     message = socket.recv()
     message = message.decode()
     print(f"Recieved message: {message}")
 
-    is_valid = is_valid_host(message)
+    response = open_url(message)
 
-    response = message + f" {is_valid}"
     socket.send(response.encode())
     print(f"Sent response: {response}")
     print()
